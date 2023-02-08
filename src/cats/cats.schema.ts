a1244f101f960d67dc1ca6eae83f673fdf40ef53
Phiip <phiip0813@naver.com>
2023-02-08 15:39:17 +0900

    created cat schema

---
+++ src/cats/cats.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { IsEmail } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+} from 'class-validator/types/decorator/decorators';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Cat extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Prop({
+    required: true,
+  })
+  password: string;
+
+  @Prop()
+  imgUrl: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
